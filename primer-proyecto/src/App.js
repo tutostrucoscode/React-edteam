ff98309a42628bc6ab997c2a2f8aa3cc56ac937f
tutostrucos <tiomix68@gmail.com>
2021-08-17 00:07:19 -0500

    Recorriendo arrays
    Recorriendo arrays

--- primer-proyecto/src/App.js
+++ primer-proyecto/src/App.js
@@ -5,6 +5,37 @@ import "bootstrap-social/bootstrap-social.css";
 import "./css/tutosgrid.css";
 import Curso from "./Curso";
 
+const cursos = [
+  {
+    titulo: "React desde cero",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/small/7204fcde-37aa-49a2-a619-63459f834ada.png",
+    price: 30,
+    profesor: "Beto Quiroga",
+  },
+  {
+    titulo: "Drupal desde cero",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/small/43973b1c-a8e2-4863-9252-2ae41377cab2.png",
+    price: 40,
+    profesor: "Beto Quiroga",
+  },
+  {
+    titulo: "Go desde cero",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/small/3ec614b5-a9b7-475c-bf8f-8ba643a129eb.png",
+    price: 50,
+    profesor: "Alexys Lozada",
+  },
+  {
+    titulo: "Html desde cero",
+    image:
+      "https://edteam-media.s3.amazonaws.com/courses/small/26557907-0555-427e-a40c-6ff207f98d72.png",
+    price: 10,
+    profesor: "Alvaro Felipe",
+  },
+];
+
 const App = () => (
   <>
     <div className="jumbotron jumbotron-fluid">
@@ -15,20 +46,17 @@ const App = () => (
     </div>
     <div className="container">
       <div className="row row-cols-md-3">
-        <Curso title="React desde cero" price={20} />
+        {cursos.map((c) => (
+          <Curso
+            title={c.titulo}
+            image={c.image}
+            price={c.price}
+            profesor={c.profesor}
+          />
+        ))}
       </div>
     </div>
   </>
 );
 
 export default App;
-
-//Reglas JSX
-//1: Toda etiqueta debe createPartialResponse
-//2: Los componentes deben devolver UN solo elemento padre
-//3: Apoyarse de los Fragment cuando necesito devolver 2 elementos
-//4: Fragment => <> hijos </>
-//5: img siempre se cierra
-//6: class => className
-//7: for => HtmlFor
-//8: No if, else, while
